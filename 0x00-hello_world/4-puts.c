83f5f858e318d914e7afe055a33c1576a3eeccb0
yegofrankline <yegofrank@gmail.com>
2022-07-02 05:37:31 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
- * main - entry point
+ * main - Entry
  *
- * Return: 0 after printing the function
+ * Return: 0 after printing function
  */
 int main(void)
-{puts("\"Programming is like building a multilingual puzzle")
-	    return (0);
+{puts("\"Programming is like a multilingual puzzle");
+	return (0);
 }
