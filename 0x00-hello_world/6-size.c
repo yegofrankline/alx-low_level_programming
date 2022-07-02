d151b8cf08e15159ac95e8850a0ab618500a3562
yegofrankline <yegofrank@gmail.com>
2022-07-02 07:00:55 -0700

    sizes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - prints the size of various types
  *
- * Return: Always 0 (Success)
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
